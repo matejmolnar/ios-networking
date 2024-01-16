9232c11689b339cea8ee884dc3a4a88600b84db7
Matej Molnar <matej.molnar@outlook.com>
2024-01-16 17:25:26 +0100

    [fix] web hosted docs

--- build_docs.sh
+++ build_docs.sh
@@ -5,4 +5,5 @@ swift package \
     generate-documentation --target Networking \
     --disable-indexing \
     --transform-for-static-hosting \
+    --hosting-base-path networking \
     --output-path ./docs
