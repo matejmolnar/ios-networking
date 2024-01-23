9ffdbcd85ee93188b2e3380fa206e51704926fa7
Matej Molnar <matej.molnar@outlook.com>
2024-01-23 14:25:52 +0100

    [feat] update build docs script

--- build_docs.sh
+++ build_docs.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
-swift package \
-    --allow-writing-to-directory ./docs \
-    generate-documentation --target Networking \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path networking \
-    --output-path ./docs
+xcodebuild \
+    -workspace Networking.xcworkspace \
+    -derivedDataPath docsData \
+    -scheme Networking \
+    -destination 'platform=iOS Simulator' \
+    -parallelizeTargets \
+    docbuild
+
+$(xcrun --find docc) process-archive transform-for-static-hosting docsData/Build/Products/Debug/Networking.doccarchive \
+    --hosting-base-path ios-networking \
+    --output-path docs/
+
+rm -rf docsData
